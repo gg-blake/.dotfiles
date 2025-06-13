c779123134e8348bbd3e817b11f3c890355adaf2
gg-blake <blake092202@gmail.com>
2025-06-13 14:43:50 -0400

    wipe for fresh config

--- nvim/.config/fish/conf.d/nix.fish
+++
@@ -1,2 +0,0 @@
-
-if test -e /home/blake/.nix-profile/etc/profile.d/nix.fish; . /home/blake/.nix-profile/etc/profile.d/nix.fish; end # added by Nix installer
--- update_env.sh
+++
@@ -1,7 +0,0 @@
-if [ -n "$CONDA_PREFIX" ]; then
-    local env_name=$(basename "$CONDA_PREFIX")
-    if [ $env_name != "miniconda3" ]; then
-        conda env update --prune --name="$env_name" --file=~/.dotfiles/conda_envs/$env_name.yml
-        echo "Environment Dependencies Updated at ~/.dotfiles/$env_name.yml"
-    fi
-fi
